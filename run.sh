06db990b36ec68687fcaf12bc8e8c26901d7fe77
DenDen047 <sh.mn.nat@gmail.com>
2021-06-16 17:10:00 +0900

    prepared the basic env

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@ IMAGE_NAME="denden047/deeplabcut"
 
 docker build -q -f "$CURRENT_PATH"/docker/Dockerfile -t ${IMAGE_NAME} . && \
 docker run -it --rm \
+    --gpus all \
     -p 2351:8888 \
     -e USER_HOME=$HOME/DeepLabCut \
     ${IMAGE_NAME} \
