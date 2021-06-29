7ef2713675743b19b14fd0920e0a6ccdfe583a43
DenDen047 <sh.mn.nat@gmail.com>
2021-06-29 12:04:25 +0900

    success to install deeplabcut[gui]

--- run.sh
+++ run.sh
@@ -3,13 +3,13 @@
 CURRENT_PATH=$(pwd)
 IMAGE_NAME="denden047/deeplabcut"
 
-docker build -q -f "$CURRENT_PATH"/docker/Dockerfile -t ${IMAGE_NAME} . && \
+docker build -f "$CURRENT_PATH"/docker/Dockerfile -t ${IMAGE_NAME} . && \
 docker run -it --rm \
     --gpus all \
     -p 2351:8888 \
-    -e USER_HOME=$HOME/DeepLabCut \
+    -p 6080:80 \
     ${IMAGE_NAME} \
-    /bin/bash
+    # /bin/bash
     # /bin/bash -c " \
     #     conda init /bin/bash && \
     #     conda activate acinoset && \
