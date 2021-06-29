c0fe327a5d14b3676764da1c6e4642f7cab24f23
DenDen047 <sh.mn.nat@gmail.com>
2021-06-29 13:42:53 +0900

    update run.sh

--- run.sh
+++ run.sh
@@ -3,9 +3,11 @@
 CURRENT_PATH=$(pwd)
 IMAGE_NAME="denden047/deeplabcut"
 
-docker build -f "$CURRENT_PATH"/docker/Dockerfile -t ${IMAGE_NAME} . && \
+docker build -q -f "$CURRENT_PATH"/docker/Dockerfile -t ${IMAGE_NAME} . && \
 docker run -it --rm \
     --gpus all \
+    -v ${CURRENT_PATH}:/root/workdir \
+    -v /data2/naoya/AcinoSet:/data \
     -p 2351:8888 \
     -p 6080:80 \
     ${IMAGE_NAME} \
