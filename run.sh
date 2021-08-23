69543f75ae98b80c8ef33c6aac0092bb35ec0fcf
DenDen047 <sh.mn.nat@gmail.com>
2021-08-23 22:30:35 +0900

    success to run without anipose

--- run.sh
+++ run.sh
@@ -3,15 +3,15 @@
 CURRENT_PATH=$(pwd)
 IMAGE_NAME="denden047/deeplabcut"
 
-docker build -q -f "$CURRENT_PATH"/docker/Dockerfile -t ${IMAGE_NAME} . && \
+docker build -f "$CURRENT_PATH"/docker/Dockerfile -t ${IMAGE_NAME} . && \
 docker run -it --rm \
-    --gpus all \
+    --gpus 0 \
     -v ${CURRENT_PATH}:/root/workdir \
-    -v /data2/naoya/AcinoSet:/data \
+    -v "$CURRENT_PATH"/data:/data \
     -p 2351:8888 \
     -p 6080:80 \
     ${IMAGE_NAME} \
-    # /bin/bash
+    /bin/bash
     # /bin/bash -c " \
     #     conda init /bin/bash && \
     #     conda activate acinoset && \
