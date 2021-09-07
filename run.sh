c897aa4256830a3248dfab14d04c14e6d08a3b75
DenDen047 <sh.mn.nat@gmail.com>
2021-09-08 01:15:02 +0900

    update for DeepLabCut 2.2

--- run.sh
+++ run.sh
@@ -6,7 +6,6 @@ IMAGE_NAME="denden047/deeplabcut"
 docker build -f "$CURRENT_PATH"/docker/Dockerfile -t ${IMAGE_NAME} . && \
 docker run -it --rm \
     --gpus 0 \
-    -v ${CURRENT_PATH}:/root/workdir \
     -v "$CURRENT_PATH"/data:/data \
     -p 2351:8888 \
     -p 6080:80 \
