598c5d2e389d98047189c4b6897f17fdf84edf2e
DenDen047 <sh.mn.nat@gmail.com>
2021-09-08 01:28:41 +0900

    add language settings

--- run.sh
+++ run.sh
@@ -6,6 +6,7 @@ IMAGE_NAME="denden047/deeplabcut"
 docker build -f "$CURRENT_PATH"/docker/Dockerfile -t ${IMAGE_NAME} . && \
 docker run -it --rm \
     --gpus 0 \
+    -v "$CURRENT_PATH"/projects:/root/workdir \
     -v "$CURRENT_PATH"/data:/data \
     -p 2351:8888 \
     -p 6080:80 \
